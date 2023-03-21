46cda779d34d4d9a8ea0c041117e382f6f3da8f7
Matth-s <senosiain.matthieu@yahoo.fr>
2023-03-21 16:56:16 +0100

    responsive article

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react";
 import ArticlePresentation from "./ArticlePresentation";
 
 const Article = ({ article }) => {
+  const [pageWidth, setPageWidth] = useState(window.innerWidth);
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    const handleResize = () => setPageWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  console.log(article);
+
+  useEffect(() => {
+    switch (true) {
+      case pageWidth > 860:
+        setImage(article.image.desktop);
+        break;
+      default:
+        setImage(article.image.mobile);
+        break;
+    }
+  }, [pageWidth]);
+
   return (
     <div className="article-container flex flex__alignCenter flex__spaceBtw">
       <div className="image-div">
         <img
           className="border-radius"
-          src={article.image.desktop}
+          src={image}
           alt={`article ${article.name}`}
         />
       </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,12 +3,16 @@ import { NavLink, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import { links } from "../../utils/Links";
 import { useSelector } from "react-redux";
+
+import Category from "../category/Category";
 import Cart from "../modal/Cart";
 
 const Header = () => {
   const cartSelector = useSelector((state) => state.cart.cart);
   const cartLength = cartSelector.length;
   const [isOpenCart, setIsOpenCart] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
+  const [pageWidth, setPageWidth] = useState(window.innerWidth);
   const location = useLocation();
   const url = location.pathname;
 
@@ -19,35 +23,66 @@ const Header = () => {
   useEffect(() => {
     function handleClickOutside(event) {
       if (event.target.className === "blur") {
-        setIsOpenCart((prev) => !prev);
+        setIsOpenCart(false);
       }
     }
-
     document.addEventListener("click", handleClickOutside);
+
+    const handleResize = () => setPageWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+
     return () => {
       document.removeEventListener("click", handleClickOutside);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
+
+  useEffect(() => {
+    if (pageWidth > 840 && openMenu) {
+      setOpenMenu(false);
+    }
+  }, [pageWidth]);
+
+  useEffect(() => {
+    if (isOpenCart || openMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isOpenCart, openMenu]);
 
   return (
     <header className="header-container flex">
       <div className="header-div flex flex__alignCenter">
+        {pageWidth <= 840 && (
+          <img
+            onClick={() => setOpenMenu((prev) => !prev)}
+            className="menu-image"
+            src="/assets/shared/tablet/icon-hamburger.svg"
+            alt="icon hamburger"
+          />
+        )}
         <img src="/assets/shared/desktop/logo.svg" alt="logo audiophile" />
-        <nav>
-          <ul className="flex">
-            {links.map((link) => (
-              <NavLink key={link.name} to={link.directed}>
-                <li
-                  className={`link ${
-                    url.includes(link.name) ? "link__active" : null
-                  }`}
-                >
-                  {link.name}
-                </li>
-              </NavLink>
-            ))}
-          </ul>
-        </nav>
+
+        {pageWidth > 840 ? (
+          <nav>
+            <ul className="flex">
+              {links.map((link) => (
+                <NavLink key={link.name} to={link.directed}>
+                  <li
+                    className={`link ${
+                      url.includes(link.name) ? "link__active" : null
+                    }`}
+                  >
+                    {link.name}
+                  </li>
+                </NavLink>
+              ))}
+            </ul>
+          </nav>
+        ) : (
+          openMenu && <Category />
+        )}
 
         <div className="imageDiv-cart" onClick={() => toggleCart()}>
           <img
@@ -58,12 +93,8 @@ const Header = () => {
           {cartLength > 0 && <span className="count">{cartLength}</span>}
         </div>
       </div>
-      {isOpenCart && (
-        <>
-          <span className="blur"></span>
-          <Cart />
-        </>
-      )}
+      {isOpenCart && <Cart />}
+      {(isOpenCart || openMenu) && <span className="blur"></span>}
     </header>
   );
 };
